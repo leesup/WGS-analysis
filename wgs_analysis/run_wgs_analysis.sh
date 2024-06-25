88040444ab9d1060c95bf87eb9db16b34010a39b
leesup <suhwanplee@gmail.com>
2024-06-25 01:59:15 -0400

    New Scripts

--- wgs_analysis/run_wgs_analysis.sh
+++ wgs_analysis/run_wgs_analysis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Path of Data Directories
-path_data = "/data/CARD/projects/Paula_WGS"
-path_ref = "/data/CARD/projects/fulyaak"
+path_data="/data/CARD/projects/Paula_WGS"
+path_ref="/data/CARD/projects/fulyaak"
 
 # Change directory to the location of run_wgs_analysis.sh
 cd "$(dirname "$0")"
@@ -26,15 +26,15 @@ echo "$gene_dict"
 module load bcftools
 
 # Split VCF by Ancestry
-bcftools view -S "${path_data}/brasil_ids.txt" "${path_data}/brasil.vcf" "${path_data}/joint-genotyping.vcf.gz"
-bcftools view -S "${path_data}/chile_ids.txt" "${path_data}/chile.vcf" "${path_data}/joint-genotyping.vcf.gz"
+bcftools view -S "${path_data}/brasil_ids.txt" --force-samples "${path_data}/joint-genotyping.vcf.gz" -o ../data/brasil.vcf
+bcftools view -S "${path_data}/chile_ids.txt" --force-samples "${path_data}/joint-genotyping.vcf.gz" -o ../data/chile.vcf
 
 # Normalize VCFs before Annotation
 # Split Multiallelic Sites into Biallelic Records
-bcftools norm -m-both -o "${path_data}/brasil_biallelic.vcf" "${path_data}/brasil.vcf"
-bcftools norm -m-both -o "${path_data}/chile_biallelic.vcf" "${path_data}/chile.vcf"
+bcftools norm -m-both ../data/brasil.vcf -o ../data/brasil_biallelic.vcf
+bcftools norm -m-both ../data/chile.vcf -o ../data/chile_biallelic.vcf
 
 # Left-Align and Normalize
-bcftools norm -f "${path_ref}/Homo_sapiens_assembly38.fasta" -o "${path_data}/brasil_normalized.vcf" "${work_dir}/brasil_biallelic.vcf"
-bcftools norm -f "${path_Ref}/Homo_sapiens_assembly38.fasta" -o "${path_data}/chile_normalized.vcf" "${work_dir}/chile_biallelic.vcf"
+bcftools norm -f "${path_ref}/Homo_sapiens_assembly38.fasta" ../data/brasil_biallelic.vcf -o ../data/brasil_normalized.vcf
+bcftools norm -f "${path_ref}/Homo_sapiens_assembly38.fasta" ../data/chile_biallelic.vcf -o ../data/chile_normalized.vcf
 
